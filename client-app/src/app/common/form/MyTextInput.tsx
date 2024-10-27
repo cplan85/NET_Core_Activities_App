56003df6151f6f5d020466b699ff81d20fe221d7
cplan85 <carlos_planchart@live.com>
2024-10-27 23:08:35 +0100

    Feat: end of section 11

---
+++ client-app/src/app/common/form/MyTextInput.tsx
@@ -0,0 +1,28 @@
+import { useField } from "formik";
+import React from "react";
+import { Form, Label } from "semantic-ui-react";
+
+interface Props {
+    placeholder: string;
+    name: string;
+    label?: string;
+}
+export default function MyTextInput(props: Props) {
+    const [field, meta] = useField(props.name);
+    return (
+        <Form.Field error={meta.touched && !!meta.error}>
+            <label>{props.label}</label>
+            <input {...field} {...props} />
+            {meta.touched && meta.error ? (
+                <Label basic color='red'>{meta.error}</Label>
+            ) : null}
+        </Form.Field>
+    )
+        
+    
+}
+
+{/* <FormField>
+<Field placeholder='Title' name='title' />
+<ErrorMessage name='title' render={error => <Label basic color='red' content={error} />} />
+</FormField> */}
