9d82036c3d92e8a622972f7d1fa3ec300aeffb22
cplan85 <carlos_planchart@live.com>
2024-12-22 17:43:27 +0100

    feat: photo widget

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -4,6 +4,7 @@ import { toast } from "react-toastify";
 import { router } from "../router/Routes";
 import { store } from "../stores/store";
 import { User, UserFormValues } from "../models/user";
+import { Profile } from "../models/profile";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -87,9 +88,25 @@ const Account = {
     register: (user: UserFormValues) => request.post<User>('/account/register', user)
 }
 
+const Profiles = {
+    get: (username: string) => request.get<Profile>(`/profiles/${username}`),
+    uploadPhoto: (file: Blob) => {
+        let formData = new FormData();
+        formData.append('File', file);
+        return axios.post('photos', formData, {
+            headers: {'Content-type': 'multipart/form-data'}
+        })
+    },
+    setMainPhoto: (id: string) => request.post(`/photos/${id}/setMain`, {}),
+    deletePhoto: (id: string) => request.del(`/photos/${id}`)
+}
+
 const agent = {
     Activities,
-    Account
+    Account,
+    Profiles
 }
+
+
 
 export default agent;
--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -3,19 +3,23 @@ import { createContext, useContext } from "react";
 import CommonStore from "./commonStores";
 import UserStore from "./userStore";
 import ModalStore from "./modalStores";
+import ProfileStore from "./profileStore";
+import { Profile } from "../models/profile";
 
 interface Store {
     activityStore: ActivityStore;
     commonStore: CommonStore;
     userStore: UserStore;
     modalStore: ModalStore;
+    profileStore: ProfileStore;
 }
 
 export const store: Store = {
     activityStore: new ActivityStore(),
     commonStore: new CommonStore(),
     userStore: new UserStore(),
-    modalStore: new ModalStore()
+    modalStore: new ModalStore(),
+    profileStore: new ProfileStore()
 }
 
 export const StoreContext = createContext(store);
